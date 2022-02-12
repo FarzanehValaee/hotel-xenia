3b39c6d79fe1c0958d25cd4145fc0ec8b909fa93
farzaneh <farzaneh.valaee79@gmail.com>
2022-02-12 04:29:47 -0800

    addslider

--- hotel-xenia.js
+++ hotel-xenia.js
@@ -1,13 +1,13 @@
-// $(document).ready(function(){
-//           $(".owl-crousel").owlCarousel({
-//                     items:3,
-//                     loop:true,
-//                     navigation:false,
-//                     dots:false,
-//                     autoplay:true,
-//                     autoplaySpeed:1000,
-//                     smartSpeed:2000,
-//                     autoplayHoverPause:true,
-//           });
-//           console.log("true")
-// });
+$(document).ready(function(){
+          $(".owl-crousel").owlCarousel({
+                    items:3,
+                    loop:true,
+                    navigation:false,
+                    dots:false,
+                    autoplay:true,
+                    autoplaySpeed:1000,
+                    smartSpeed:2000,
+                    autoplayHoverPause:true,
+          });
+          console.log("true")
+});
