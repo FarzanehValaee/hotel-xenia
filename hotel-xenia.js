dd185ed2f8b86b77372ed18e60cc6a2f16cf0aa9
farzaneh <farzaneh.valaee79@gmail.com>
2022-02-13 00:18:27 -0800

    add navBar

--- hotel-xenia.js
+++ hotel-xenia.js
@@ -35,45 +35,45 @@
 
 // ______________________________________
 $(function () {
-          let SHOW_NUM = 0;
-          const carouselNextNew = (prev) => {
+          let sliderIndex = 0;
+          const showSlide = (prev) => {
               const scroll = $(".slidePart");
-              const shows =$(".slide");
+              const sliders =$(".slide-show");
               if (prev) {
-                  SHOW_NUM = ( --SHOW_NUM ) % shows.length;
-                  if (SHOW_NUM < 0) {
-                      SHOW_NUM += shows.length;
+                  sliderIndex = ( --sliderIndex ) % sliders.length;
+                  if (sliderIndex < 0) {
+                      sliderIndex += sliders.length;
                   }
               } else {
-                  SHOW_NUM = ( ++SHOW_NUM ) % shows.length;
+                  sliderIndex = ( ++sliderIndex ) % sliders.length;
               }
-              scroll.css(`transform`,` translateX(-${SHOW_NUM}00%);` )
+              scroll.css(`transform`,` translateX(-${sliderIndex}00%);` )
           }
 })
 $(function () {
           function next() {
-              let carousel = $('.slidePart .next').parents('.slidePart')
-              let index = $(carousel).find('.slide.active').index()
-              let content = $(carousel).find('.slide-show')
-              const length_items =  $(carousel).find('.slide').length
+              let slider = $('.slidePart .next').parents('.slidePart')
+              let index = $(slider).find('.slide.active').index()
+              let content = $(slider).find('.slide-show')
+              const length_items =  $(slider).find('.slide').length
               if (index < length_items-1 ){
                   $(content).css('transform', `translate(${(index+1)*-100}%)`)
-                  $(carousel).find('.slide.active').removeClass('active')
-                  $(carousel).find('.slide').eq(index+1).addClass('active')
+                  $(slider).find('.slide.active').removeClass('active')
+                  $(slider).find('.slide').eq(index+1).addClass('active')
               }
           }
           $('.slidePart .next').click(next)
   
   
           $('.slidePart .prev').click(function () {
-              let carousel = $(this).parents('.slidePart')
-              let index = $(carousel).find('.slide.active').index()
-              let content = $(carousel).find('.slide-show')
+              let slider = $(this).parents('.slidePart')
+              let index = $(slider).find('.slide.active').index()
+              let content = $(slider).find('.slide-show')
               if (index > 0 ){
                   console.log({index})
                   $(content).css('transform', `translate(${(index-1)*-100}%)`)
-                  $(carousel).find('.slide.active').removeClass('active')
-                  $(carousel).find('.slide').eq(index-1).addClass('active')
+                  $(slider).find('.slide.active').removeClass('active')
+                  $(slider).find('.slide').eq(index-1).addClass('active')
               }
           })
 })
